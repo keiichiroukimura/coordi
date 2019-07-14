7a9ea10975972f2e2f4e796298a733220d0544df
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-07-14 20:07:59 +0900

    fix: clothesmodel実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_234134) do
+ActiveRecord::Schema.define(version: 2019_07_14_110222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clothes", force: :cascade do |t|
+    t.text "image_first"
+    t.text "image_second"
+    t.text "image_third"
+    t.integer "gender"
+    t.integer "height"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_clothes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_07_13_234134) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "clothes", "users"
 end

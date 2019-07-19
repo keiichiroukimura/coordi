bbff084dce69b3485a16702cb0bb04757933c9c6
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-07-19 11:34:55 +0900

    add: seedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Label.create!(content: "アクティブ")
+Label.create!(content: "海遊び")
+Label.create!(content: "山遊び")
+Label.create!(content: "スポーツ")
+Label.create!(content: "公園")
+Label.create!(content: "街中カジュアル")
+Label.create!(content: "フォーマル")
+Label.create!(content: "雪遊び")
 
-Label.create!(content: "和装")
-
-Label.create!(content: "カジュアル")
 

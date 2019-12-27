9fdd43f8dc4a47172920fd271780483a8a447969
木村　圭一郎 <kimurakeiichirou@kimurakeiichirounoMacBook-Pro.local>
2019-12-27 14:32:18 +0900

    確認画面s3対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,9 @@
-CarrierWave.configure do |config|
-  require 'carrierwave/storage/abstract'
-  require 'carrierwave/storage/file'
-  require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
+CarrierWave.configure do |config|
+  
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -19,9 +20,8 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'coordi-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/coordi-production'
+      
     when 'development'
       config.fog_directory = 'coordi-production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/coordi-production'
   end
 end
